37f74e24d4e00f57e476461bb2b5662793341dff
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-08-27 08:42:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Notes-taking-app
-xd
+# Notes taking app
+This application is a very awesome notes app which will help to finish your work at time
+
+[Open notes taking app](https://muhammedraiyaan2.github.io/Notes-taking-app)
+# How to use the application
+It's simple to use first you have to enter the title of the note and the body of the note after the you have to click on submit it save your note on local storage and the data is save on you computer and if you want to delete a note and click on the delete option in the note of the bottom
+# Contact
+If you want to contact us go to the website and there in the right side of the header there is a section contact and there you can get the contact details
---
+++ logic.js
@@ -0,0 +1,80 @@
+let btn = document.getElementById('btn')
+let text = document.getElementById('text')
+let head=document.getElementById('head')
+//show note function
+localStorage.setItem('head',head.value)
+function showNotes() {
+ let notes = localStorage.getItem('notes')
+ if (notes == null) {
+  notesObj = []
+ }
+ else {
+  notesObj = JSON.parse(notes)
+ }
+ let html = ``
+ notesObj.forEach(function (element, index) {
+  html += `
+  <div class="noteCard card m-3" style="width: 18rem;">
+  <!-- <img src="..." class="card-img-top" alt="..."> -->
+  <div class="card-body">
+   <h3 class="card-title">${element.title}</h3>
+   <p class="card-text">${element.text}</p>
+   <button onclick="to_delete(this.id)" id="${index}" class="btn btn-primary">Delete</button>
+  </div>
+</div>
+  `
+  let notesEle = document.getElementById(`notes`)
+  if (notesObj.length != 0) {
+   notesEle.innerHTML = html;
+  }
+  else {
+   notesEle.innerHTML = `<h1>nothing is there in notes!</h1>`
+  }
+ });
+}
+showNotes()
+//If user adds a note add it to the local storage
+btn.addEventListener("click", function () {
+ let notes = localStorage.getItem('notes')
+ if (notes == null) {
+  notesObj = []
+ }
+ else {
+  notesObj = JSON.parse(notes)
+ }
+ let myObj={
+  title:head.value,
+  text:text.value
+ }
+ head.value=""
+ text.value = ""
+ notesObj.push(myObj)
+ localStorage.setItem(`notes`, JSON.stringify(notesObj))
+ showNotes()
+})
+function to_delete(index) {
+ let notes = localStorage.getItem('notes')
+ if (notes == null) {
+  notesObj = []
+ }
+ else {
+  notesObj = JSON.parse(notes)
+ }
+ notesObj.splice(index, 1)
+ localStorage.setItem(`notes`, JSON.stringify(notesObj))
+ showNotes()
+}
+let search = document.getElementById(`search`)
+search.addEventListener('input', function () {
+ let inputVal = search.value
+ let noteCards = document.getElementsByClassName(`noteCard`)
+ Array.from(noteCards).forEach(function (element) {
+  let cardTxt = element.getElementsByTagName(`p`)[0].innerText
+  if (cardTxt.includes(inputVal)) {
+   element.style.display = "block"
+  }
+  else {
+   element.style.display = "none"
+  }
+ });
+})
